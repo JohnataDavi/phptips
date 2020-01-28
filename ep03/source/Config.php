c673758f0e5d8be5a4cf097974af2262a4cff11a
Johnata Davi <infojohnata@gmail.com>
2020-01-27 22:04:29 -0300

    Create Config.php

---
+++ ep03/source/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+// HOST SMTP
+define("MAIL", [
+    "host" => "smtp.gmail.com",
+    "port" => "587",
+    "user" => "suporteniconimes@gmail.com",
+    "passwd" => "peledecavalo",
+    "from_name" => "Johnata Davi",
+    "from_email" => "infojohnata@gmail.com"
+]);
