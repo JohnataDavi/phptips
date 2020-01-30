f1007d3a8b6f03dd07dbf6e4f0369d437039ae84
Johnata Davi <infojohnata@gmail.com>
2020-01-30 15:06:56 -0300

    Create index.php

---
+++ ep05/index.php
@@ -0,0 +1,27 @@
+<?php
+
+require __DIR__."/vendor/autoload.php";
+
+use CoffeeCode\Paginator\Paginator;
+use Source\Models\Post;
+
+$post = new Post();
+$page = filter_input(INPUT_GET, "page", FILTER_SANITIZE_STRIPPED);
+
+
+//$paginator = new Paginator("http://localhost/phptips/ep05/?page=", "Página", ["Primeira Página", "Primeira"],["Última Página", "Última"]);
+$paginator = new Paginator("http://localhost/phptips/ep05/?page=");
+$paginator->pager($post->find()->count(), 3, $page, 2);
+
+$posts = $post->find()->limit($paginator->limit())->offset($paginator->offset())->fetch(true);
+
+echo "<link rel='stylesheet' href='style.css'/>";
+echo "<p>Página {$paginator->page()} de {$paginator->pages()}</p>";
+
+if($posts){
+    foreach ($posts as $post) {
+        echo "<article class='post'><img src='{$post->cover}'/><div><h1>{$post->title}</h1><div>{$post->description}</div></div></article><br>";
+    }
+}
+
+echo $paginator->render();
