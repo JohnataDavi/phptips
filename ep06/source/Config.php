272a51131bce1719b77a63836778c7926ef7f0c0
Johnata Davi <infojohnata@gmail.com>
2020-01-31 15:23:37 -0300

    Create Config.php

---
+++ ep06/source/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+session_start();
+
+define("FACEBOOK", [
+    "app_id" => "593307304784277",
+    "app_secret" => "a034d4df36c7d9361f32d03b143d72b8",
+    "app_redirect" => "http://localhost/phptips/ep06",
+    "app_version" => "v5.0"
+]);
